8f1c8b5da1ae438518e9b67acbe65cbd1171ab5e
surenderthakran <surender.thakran89@gmail.com>
2019-05-01 10:52:33 +0530

    Updated api's App.java to remove all external dependencies

--- src/main/java/com/surenderthakran/kubaer/api/App.java
+++ src/main/java/com/surenderthakran/kubaer/api/App.java
@@ -1,11 +1,7 @@
 package com.surenderthakran.kubaer.api;
 
-import com.google.common.flogger.FluentLogger;
-
 class App {
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-
   public static void main(String[] args) {
-    logger.atInfo().log("Hello World!");
+    System.out.println("Hello World!");
   }
 }
