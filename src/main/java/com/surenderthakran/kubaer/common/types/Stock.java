a86a3fc1ef2d4083f9864d8ada316b51de8338a8
surenderthakran <surender.thakran89@gmail.com>
2019-05-02 09:03:05 +0530

    Added Stock type.

---
+++ src/main/java/com/surenderthakran/kubaer/common/types/Stock.java
@@ -0,0 +1,11 @@
+package com.surenderthakran.kubaer.common.types;
+
+public class Stock {
+  private final String symbol;
+  private final String name;
+
+  public Stock(String symbol, String name) {
+    this.symbol = symbol;
+    this.name = name;
+  }
+}
