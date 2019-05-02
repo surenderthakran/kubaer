6bd0c6c6ffb2e2a8f6c3587b728045da1477a8e7
surenderthakran <surender.thakran89@gmail.com>
2019-05-02 09:03:55 +0530

    Initialized API's App class as spring boot application.

--- src/main/java/com/surenderthakran/kubaer/api/App.java
+++ src/main/java/com/surenderthakran/kubaer/api/App.java
@@ -1,11 +1,16 @@
 package com.surenderthakran.kubaer.api;
 
 import com.google.common.flogger.FluentLogger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 class App {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   public static void main(String[] args) {
     logger.atInfo().log("Hello World!");
+
+    SpringApplication.run(App.class, args);
   }
 }
