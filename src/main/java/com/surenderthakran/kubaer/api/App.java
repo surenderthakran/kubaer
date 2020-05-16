be6c4c254a4362482cd1984ef49a8703cefd0cd3
surenderthakran <surender.thakran89@gmail.com>
2020-05-16 10:04:20 +0530

    Added local mongo connection

--- src/main/java/com/surenderthakran/kubaer/api/App.java
+++ src/main/java/com/surenderthakran/kubaer/api/App.java
@@ -1,6 +1,12 @@
 package com.surenderthakran.kubaer.api;
 
 import com.google.common.flogger.FluentLogger;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +16,16 @@ public class App {
 
   public static void main(String[] args) {
     logger.atInfo().log("Hello World!");
+
+    // MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://172.17.0.1:27017"));
+    MongoClient mongoClient = new MongoClient(new ServerAddress("172.17.0.1", 27017));
+    DB database = mongoClient.getDB("dev");
+
+    DBCollection collection = database.getCollection("stocks");
+    BasicDBObject document = new BasicDBObject();
+    document.put("symbol", "TEST");
+    document.put("name", "Test Stock");
+    collection.insert(document);
 
     SpringApplication.run(App.class, args);
   }
